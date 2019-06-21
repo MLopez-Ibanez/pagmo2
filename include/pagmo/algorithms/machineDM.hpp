391c17f763b516d3aaa93b33c69e9d70b0239fff
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-06-21 15:53:39 +0100

    Add skeleton of machineDM.

---
+++ include/pagmo/algorithms/machineDM.hpp
@@ -0,0 +1,97 @@
+#ifndef PAGMO_ALGORITHMS_MACHINEDM_HPP
+#define PAGMO_ALGORITHMS_MACHINEDM_HPP
+
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include <pagmo/detail/visibility.hpp>
+#include <pagmo/problem.hpp>
+#include <pagmo/population.hpp>
+#include <pagmo/rng.hpp>
+
+namespace pagmo
+{
+class PAGMO_DLL_PUBLIC value_function
+{
+public:
+
+    virtual double value(const std::vector<double> &) const = 0;
+    std::vector<double> weights;
+
+protected:
+    value_function(std::vector<double> w)
+        : weights(w)
+    {};
+};
+
+class PAGMO_DLL_PUBLIC linear_value_function : public value_function
+{
+public:
+
+    linear_value_function(std::vector<double> w)
+        : value_function(w)
+    {};
+    double value(const std::vector<double> &) const;
+};
+
+/// Machine Decision Maker
+/**
+ * FIXME: DOCUMENT ME !
+ */
+class PAGMO_DLL_PUBLIC machineDM
+{
+public:
+    /// Constructor
+    /**
+     * FIXME: Update documentation: Constructs a Machine Decision Maker
+     *
+     * @param[in] gen Number of generations to evolve.
+     * @param[in] cr Crossover probability.
+     * @param[in] eta_c Distribution index for crossover.
+     * @param[in] m Mutation probability.
+     * @param[in] eta_m Distribution index for mutation.
+     * @param seed seed used by the internal random number generator (default is random)
+     * @throws std::invalid_argument if \p cr is not \f$ \in [0,1[\f$, \p m is not \f$ \in [0,1]\f$, \p eta_c is not in
+     * [1,100[ or \p eta_m is not in [1,100[.
+     */
+    machineDM(problem &prob, value_function &pref,
+              unsigned seed = pagmo::random_device::next())
+        : prob(prob), pref(pref)
+    {};
+
+    /**
+     * Evaluate fitness (objective vector) according to decision maker.
+     *
+     *
+     **/
+    vector_double fitness(const vector_double &) const;
+
+    /**
+     * Solution value according to DM.
+     *
+     *
+     **/
+    double value(const vector_double &) const;
+
+    /**
+     * True solution value according to DM's preference function.
+     *
+     *
+     **/
+    double true_value(const vector_double &) const;
+
+    std::vector<size_t> rank(const pagmo::population &pop) const;
+
+    template <typename Archive> void serialize(Archive &ar, unsigned);
+
+    value_function & pref;
+    problem &prob;    
+};
+
+} // namespace pagmo
+
+// FIXME: Not working
+//PAGMO_S11N_ALGORITHM_EXPORT_KEY(pagmo::machineDM)
+
+#endif
