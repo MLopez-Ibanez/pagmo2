6f5f32d32e65a91f0386e1e266978a664cb87300
ShavaraniMahdi <shavarani.mahdi@gmail.com>
2019-09-13 19:36:26 +0100

    13092019

--- include/pagmo/algorithms/machineDM.hpp
+++ include/pagmo/algorithms/machineDM.hpp
@@ -6,8 +6,8 @@
 #include <vector>
 
 #include <pagmo/detail/visibility.hpp>
-#include <pagmo/problem.hpp>
 #include <pagmo/population.hpp>
+#include <pagmo/problem.hpp>
 #include <pagmo/rng.hpp>
 
 namespace pagmo
@@ -15,155 +15,169 @@ namespace pagmo
 class PAGMO_DLL_PUBLIC value_function
 {
 public:
-
     virtual double value(const std::vector<double> &) const = 0;
     std::vector<double> weights;
     std::vector<double> ideal_point;
 
 protected:
-    value_function(std::vector<double> w,std::vector<double> ip={0,0})
-        : weights(w),ideal_point(ip)
-    {};
+    value_function(std::vector<double> w, std::vector<double> ip = {0, 0}) : weights(w), ideal_point(ip){};
 };
+// moved tthe steart as a function to machineDM
+// class PAGMO_DLL_PUBLIC stewart_value_function : public value_function
+// {
+// public:
+//     stewart_value_function(std::vector<double> w) : value_function(w){};
+//     double value(const vector_double &obj, const vector_double &alpha, const vector_double &beta,
+//                  const vector_double &lambda, const vector_double &tau) const;
+// };
 
-class PAGMO_DLL_PUBLIC linear_value_function : public value_function //Mahdi: The value function is protected, why we have public here?
+class PAGMO_DLL_PUBLIC linear_value_function
+    : public value_function // Mahdi: The value function is protected, why we have public here?
 {
 public:
-
-    linear_value_function(std::vector<double> w)
-        : value_function(w)
-    {};
+    linear_value_function(std::vector<double> w) : value_function(w){};
     double value(const std::vector<double> &) const;
 };
 
-
-class PAGMO_DLL_PUBLIC poly_value_function : public value_function //Mahdi: The value function is protected, why we have public here?
+class PAGMO_DLL_PUBLIC poly_value_function
+    : public value_function // Mahdi: The value function is protected, why we have public here?
 {
 private:
-std::vector <double> weights;
-std::vector<std::vector<int>> degrees;
+    std::vector<double> weights;
+    std::vector<std::vector<int>> degrees;
 
 public:
-
-    poly_value_function(std::vector<double> w)
-        : value_function(w)
+    poly_value_function(std::vector<double> w) : value_function(w)
     {
-      static const size_t npos = -1;
-    	std::string str;
-    	int obfs;
-    	std::cout << "      Enter a polynomial without like terms\n";
-    	std::cout << "(use the letter x. for ex.: -x1^4+x2X3^4)\n";
-    	std::cout << "\nEnter: ";
-    	std::cin >> str;
-    	if(str == "") {std::cout<<"No function is defined\n";}
-    	int strSize = str.size();
-    	std::cout << "Enter total number of objectives/criteria\n";
-    	std::cout << "\nEnter: ";
-    	std::cin >> obfs;
+        static const size_t npos = -1;
+        std::string str;
+        int obfs;
+        std::cout << " Enter a polynomial without like terms\n";
+        std::cout << "(use the letter x. for ex.: -x1^4+x2X3^4)\n";
+        std::cout << "\nEnter: ";
+        std::cin >> str;
+        if (str == "") {
+            std::cout << "No function is defined\n";
+        }
+        int strSize = str.size();
+        std::cout << "Enter total number of objectives/criteria\n";
+        std::cout << "\nEnter: ";
+        std::cin >> obfs;
 
-    //	How many monomials has the polynomial?
-    	int k = 1;
-    	for(int i = 1; i < strSize; ++i)
-    		if(str[i] == '+' || str[i] == '-')
-    			k++;
-     	int monoms = k ;
+        //	How many monomials has the polynomial?
+        int k = 1;
+        for (int i = 1; i < strSize; ++i)
+            if (str[i] == '+' || str[i] == '-') k++;
+        int monoms = k;
 
-    //	Signs "+" are necessary for the string parsing
-    	if(isdigit(str[0])) str.insert(0, "+");
-    	if(str[0] == 'x') str.insert(0, "+");
-    	str.append("+");
-    	strSize = str.size();
+        //	Signs "+" are necessary for the string parsing
+        if (isdigit(str[0])) str.insert(0, "+");
+        if (str[0] == 'x') str.insert(0, "+");
+        str.append("+");
+        strSize = str.size();
 
-    //	Extracting the monomials as monomStr
-    	k = 0;
-    	int j = 0;
-    	std::string monomStr[monoms];
-    	for(int i = 1; i < strSize; ++i)
-    		if(str[i] == '+' || str[i] == '-')
-    		{
-    			monomStr[k++] = str.substr(j, i - j);
-    			j = i;
-    		}
+        //	Extracting the monomials as monomStr
+        k = 0;
+        int j = 0;
+        std::string monomStr[monoms];
+        for (int i = 1; i < strSize; ++i)
+            if (str[i] == '+' || str[i] == '-') {
+                monomStr[k++] = str.substr(j, i - j);
+                j = i;
+            }
 
+        //  Monomials' formatting i.e. to have all the same form: coefficientX^exponent
+        for (int i = 0; i < monoms; ++i) {
+            if (monomStr[i][1] == 'x')      // x is after the +/- sign
+                monomStr[i].insert(1, "1"); //& gets 1 as coefficient
+            bool flag = false;              // assuming that x is not present
+            int len = monomStr[i].size();
+            for (int j = 1; j < len; ++j)
+                if (monomStr[i][j] == 'x') // but we test this
+                {
+                    flag = true;                  //& if x is present
+                    if (j == len - 1)             //& is the last
+                        monomStr[i].append("^1"); // it gets exponent 1
+                    break;                        //& exit from j_loop
+                }
+            if (!flag)                     // if x is not present: we have a constant term
+                monomStr[i].append("x^0"); // who gets "formatting"
+        }
 
-    //  Monomials' formatting i.e. to have all the same form: coefficientX^exponent
-    	for(int i = 0; i < monoms; ++i)
-    	{
-    		if(monomStr[i][1] == 'x')	//x is after the +/- sign
-    			monomStr[i].insert(1, "1");	//& gets 1 as coefficient
-    		bool flag = false;	// assuming that x is not present
-    		int len = monomStr[i].size();
-    		for(int j = 1; j < len; ++j)
-    			if(monomStr[i][j] == 'x')	//but we test this
-    			{
-    				flag = true;	//& if x is present
-    				if(j == len - 1)	//& is the last
-    					monomStr[i].append("^1");	//it gets exponent 1
-    				break;	//& exit from j_loop
-    			}
-    		if(!flag)	//if x is not present: we have a constant term
-    			monomStr[i].append("x^0");	//who gets "formatting"
-    	}
+        // extracting weights and degrees from monomStr
+        weights.resize(monoms, 1); // we have monoms weights
 
+        // extraction of weights
+        for (int i = 0; i < monoms; i++) {
+            if (monomStr[i].find('x')
+                == std::string::npos) { // because of some errors I had to replace string::npos with -1;
+                weights[i] = stoi(monomStr[i]);
+                continue;
+            }
+            for (int j = 0; j < monomStr[i].size(); i++) {
+                if (monomStr[i][j] == 'x' || monomStr[i][j] == 'X') {
+                    if (j == 1) {
+                        break;
+                    } else {
+                        weights[i] = stoi(monomStr[i].substr(0, j - 1));
+                        break;
+                    }
+                }
+            }
+        }
 
-    	//extracting weights and degrees from monomStr
-    	weights.resize(monoms,1); //we have monoms weights
-
-
-    	// extraction of weights
-    	for (int i=0; i<monoms;i++){
-    		if (monomStr[i].find('x')==std::string::npos){			//because of some errors I had to replace string::npos with -1;
-    			weights[i]=stoi(monomStr[i]);
-    			continue;}
-    		for (int j=0; j<monomStr[i].size(); i++){
-    			if(monomStr[i][j] == 'x' || monomStr[i][j] == 'X'){
-    				if (j==1)
-    				{ break;
-    				}
-    				else{
-    				weights[i]=stoi(monomStr[i].substr(0,j-1));
-    				break;
-    			}
-    		}
-    	}
-    }
-
-    	//extractiion of degress
-    	 						//degrees has monoms elemets (for each monomial of the polonomial), each element is a vector of m elements
-    	degrees.resize(monoms,std::vector<int> (obfs));		//where m is the number of obfs each of the m elements specify the degree of that obf in this monomial
-    	for (int i=0; i<monoms;i++){
-    	j=0;
-    	int xindex1, xindex2, pindex, o;
-    	while (j<monomStr[i].size()){
-    		if (monomStr[i].find('x')==std::string::npos){break;}
-    		xindex2=monomStr[i].find('x',xindex1+1);
-    		pindex=monomStr[i].find('^',xindex1+1);
-    		o=stoi(monomStr[i].substr(xindex1+1,std::min(xindex2,pindex)));
-    		degrees[i][o]=1; j=std::min(xindex2,pindex);
-    		if (xindex2>pindex ){
-    			degrees[i][o]=stoi(monomStr[i].substr(pindex+1,xindex2));
-    			j=xindex2;
-    		}
-    	}
-    }
+        // extractiion of degress
+        // degrees has monoms elemets (for each monomial of the polonomial), each element is a vector of m elements
+        degrees.resize(monoms, std::vector<int>(obfs)); // where m is the number of obfs each of the m elements specify
+                                                        // the degree of that obf in this monomial
+        for (int i = 0; i < monoms; i++) {
+            j = 0;
+            int xindex1, xindex2, pindex, o;
+            while (j < monomStr[i].size()) {
+                if (monomStr[i].find('x') == std::string::npos) {
+                    break;
+                }
+                xindex2 = monomStr[i].find('x', xindex1 + 1);
+                pindex = monomStr[i].find('^', xindex1 + 1);
+                o = stoi(monomStr[i].substr(xindex1 + 1, std::min(xindex2, pindex)));
+                degrees[i][o] = 1;
+                j = std::min(xindex2, pindex);
+                if (xindex2 > pindex) {
+                    degrees[i][o] = stoi(monomStr[i].substr(pindex + 1, xindex2));
+                    j = xindex2;
+                }
+            }
+        }
     };
     double value(const std::vector<double> &) const;
 };
 
-
 class PAGMO_DLL_PUBLIC quadratic_value_function : public value_function
 {
 public:
-quadratic_value_function ( std::vector<double> w, std::vector<double> ip)
-        : value_function(w,ip)//maybe we can delete the ip(idealpoint) from the input, get it in the constructor by pgamo::ideal(get_f)
-{};
-double value(const std::vector<double> &) const;
+    quadratic_value_function(std::vector<double> w, std::vector<double> ip)
+        : value_function(w, ip) // maybe we can delete the ip(idealpoint) from the input, get it in the constructor by
+                                // pgamo::ideal(get_f)
+          {};
+    double value(const std::vector<double> &) const;
 };
+
+class PAGMO_DLL_PUBLIC tchebycheff_value_function : public value_function
+{
+public:
+    tchebycheff_value_function(std::vector<double> w, std::vector<double> ip)
+        : value_function(w, ip) // maybe we can delete the ip(idealpoint) from the input, get it in the constructor by
+                                // pgamo::ideal(get_f)
+          {};
+    double value(const std::vector<double> &) const;
+};
+
 /// Machine Decision Maker
 /**
  * FIXME: DOCUMENT ME !
  */
-class PAGMO_DLL_PUBLIC machineDM
+class PAGMO_DLL_PUBLIC machineDM // M: we can bring all the parameters such as alpha, beta , sigam and etc here in the
+                                 // parameters of machineDM
 {
 public:
     /// Constructor
@@ -179,10 +193,8 @@ public:
      * @throws std::invalid_argument if \p cr is not \f$ \in [0,1[\f$, \p m is not \f$ \in [0,1]\f$, \p eta_c is not in
      * [1,100[ or \p eta_m is not in [1,100[.
      */
-    machineDM(problem &prob, value_function &pref,
-              unsigned seed = pagmo::random_device::next())
-        : prob(prob), pref(pref)
-    {};
+    machineDM(problem &prob, value_function &pref, unsigned seed = pagmo::random_device::next())
+        : prob(prob), pref(pref){};
 
     /**
      * Evaluate fitness (objective vector) according to decision maker.
@@ -190,6 +202,18 @@ public:
      *
      **/
     vector_double fitness(const vector_double &) const;
+    /* Select a subset of q criteria from m true criteria with probability
+   proportional to their weights.  */
+    vector_double select_criteria_subset(size_t q);
+    int roulette_wheel(vector_double &);
+    vector_double modify_criteria(vector_double &obf, const std::vector<int> &c, int q, double gamma);
+
+    double estim_valuefunc(vector_double &obj, const vector_double &alpha, const vector_double &beta,
+                           const vector_double &lambda, double gamma, double sigma, double delta, int q);
+
+    double stewart_value_function(const vector_double &obj, const vector_double &alpha, const vector_double &beta,
+                                  const vector_double &lambda, const vector_double &tau) const;
+    double Rand_normal(double mean, double sd);
 
     /**
      * Solution value according to DM.
@@ -207,15 +231,16 @@ public:
 
     std::vector<size_t> rank(const pagmo::population &pop) const;
 
-    template <typename Archive> void serialize(Archive &ar, unsigned);
+    template <typename Archive>
+    void serialize(Archive &ar, unsigned);
 
-    value_function & pref;
+    value_function &pref;
     problem &prob;
 };
 
 } // namespace pagmo
 
 // FIXME: Not working
-//PAGMO_S11N_ALGORITHM_EXPORT_KEY(pagmo::machineDM)
+// PAGMO_S11N_ALGORITHM_EXPORT_KEY(pagmo::machineDM)
 
 #endif
--- src/algorithms/machineDM.cpp
+++ src/algorithms/machineDM.cpp
@@ -11,8 +11,8 @@
 #include <utility>
 #include <vector>
 
-#include <boost/range/combine.hpp>
 #include <boost/foreach.hpp>
+#include <boost/range/combine.hpp>
 
 #include <pagmo/algorithm.hpp>
 #include <pagmo/algorithms/machineDM.hpp>
@@ -27,7 +27,7 @@
 namespace pagmo
 {
 
-double linear_value_function::value(const std::vector<double> & obj) const
+double linear_value_function::value(const std::vector<double> &obj) const
 {
     double value = 0.;
     // C++17:
@@ -36,47 +36,212 @@ double linear_value_function::value(const std::vector<double> & obj) const
          }
     */
     double w, o;
-    BOOST_FOREACH(boost::tie(w,o), boost::combine(this->weights, obj)) {
+    BOOST_FOREACH (boost::tie(w, o), boost::combine(this->weights, obj)) {
         value += w * o;
     }
     return value;
 }
-double quadratic_value_function::value(const std::vector<double> & obj) const
+double quadratic_value_function::value(const std::vector<double> &obj) const
 {
     double value = 0.;
     double w, ip, o;
-    BOOST_FOREACH(boost::tie(w, ip, o) , boost::combine(this->weights, this->ideal_point, obj)) {
-        value += pow(w * (o-ip),2);
+    BOOST_FOREACH (boost::tie(w, ip, o), boost::combine(this->weights, this->ideal_point, obj)) {
+        value += pow(w * (o - ip), 2);
     }
     return value;
-
 }
 
-vector_double
-machineDM::fitness(const vector_double & solution) const
+double tchebycheff_value_function::value(const std::vector<double> &obj) const
+{
+    double temp;
+    double w, ip, o;
+    double value = 0.;
+    BOOST_FOREACH (boost::tie(w, ip, o), boost::combine(this->weights, this->ideal_point, obj)) {
+        temp = w * std::abs(o - ip);
+        if (temp > value) {
+            value = temp;
+        }
+    }
+    return value;
+}
+/* Select a subset of q criteria from m true criteria with probability
+   proportional to their weights.  */
+vector_double machineDM::select_criteria_subset(size_t q)
+{
+    vector_double w = this->pref.weights;
+    vector_double c;
+    size_t m = w.size();
+    double sum = accumulate(w.begin(), w.end(), 0);
+    vector_double probs(m);
+    for (size_t i = 0; i < m; i++) {
+        probs[i] = w[i] / sum; // M: instead of ranking the elements based on weights, I directly use weights to
+                               // calculate the prob. There is no need to rank them I think
+    }
+
+    for (size_t i = 0; i < m; i++) {
+        int k = roulette_wheel(probs);
+        c[i] = k;
+        probs[k] = 0.0;
+    }
+}
+
+// This roulette_wheel function returns an index of an element based on the
+// probabilities constructed based on the values of the passed vector
+int machineDM::roulette_wheel(vector_double &v)
+{
+    double sum = 0.;
+    double rndNumber;
+    int i;
+    for (i = 0; i < v.size(); i++) {
+        sum += v[i];
+    }
+    srand((unsigned)time(NULL)); // M: I'm not sure if this line randomizes the seed or if we should randomize it at all
+    rndNumber = ((double)rand() / (double)RAND_MAX) * sum;
+    for (i = 0; i < v.size(); i++) {
+        if (v[i] >= rndNumber) {
+            return i;
+        }
+    }
+}
+vector_double machineDM::modify_criteria(
+    vector_double &obf, const std::vector<int> &c, int q,
+    double gamma) // M: I have assumed that tau in previous vrsion is ideal point. thus I have changed it
+{
+    assert(gamma >= 0);
+    assert(gamma < 1);
+    int m = obf.size();
+    vector_double ip = this->pref.ideal_point;
+    vector_double zhat(m);
+    for (int k = 1; k < q; k += 2) {
+        assert(k < m);
+        assert(c[k - 1] >= 0);
+        assert(c[k - 1] < m);
+        assert(c[k] >= 0);
+        assert(c[k] < m);
+        zhat[k - 1] = (1.0 - gamma) * obf[c[k - 1]] + gamma * obf[c[k]];
+        zhat[k] = (1.0 - gamma) * obf[c[k]] + gamma * obf[c[k - 1]];
+    }
+    // if q is odd.
+    if (q % 2) zhat[q - 1] = obf[c[q - 1]];
+
+    /*
+       (a) the m - q unmodelled criteria set at their
+       reference levels tau_i (i.e. no perceived gains or
+       losses)
+    */
+
+    for (int i = q; i < m; i++) {
+        zhat[i] = ip[c[i]];
+    }
+
+    return zhat;
+}
+double machineDM::stewart_value_function(const vector_double &obj, const vector_double &alpha,
+                                         const vector_double &beta, const vector_double &lambda,
+                                         const vector_double &tau) const
+{
+    double sum = 0.0;
+    double st;
+    vector_double w = this->pref.weights;
+    for (int i = 0; i < obj.size(); i++) {
+        if (obj[i] <= tau[i]) {
+            st = lambda[i] * (exp(alpha[i] * obj[i]) - 1.0) / (exp(alpha[i] * tau[i]) - 1.0);
+        } else {
+            st = lambda[i]
+                 + (1.0 - lambda[i]) * (1.0 - exp(-beta[i] * (obj[i] - tau[i])))
+                       / (1.0 - exp(-beta[i] * (1.0 - tau[i])));
+        }
+        sum += w[i] * st;
+    }
+    return sum;
+}
+
+double machineDM::estim_valuefunc(vector_double &obj, const vector_double &alpha, const vector_double &beta,
+                                  const vector_double &lambda, double gamma, double sigma, double delta, int q)
+{
+    int m = obj.size();
+    vector_double tau = this->pref.ideal_point; // M: I'm not sure if this is a right way to access the ideal_point
+    std::vector<int> c(m);
+
+    if (q < m) {
+        machineDM::select_criteria_subset(q);
+    } else {
+        assert(q == m);
+        for (size_t i = 0; i < m; i++) {
+            c[i] = i;
+        }
+    }
+
+    vector_double z_mod = machineDM::modify_criteria(obj, c, q, gamma);
+
+    /*
+      (b) the addition of a noise term, normally
+      distributed with zero mean and a variance of
+      sigma^2 (which will be a specified model parameter),
+    */
+    double noise = machineDM::Rand_normal(0.0, sigma * sigma);
+
+    /* (c) a shift in the reference levels tau_i from the ‘ideal’
+       positions by an amount \delta, which may be
+       positive or negative (and which is also a
+       specified model parameter).
+    */
+    vector_double tau_mod(m);
+    for (int i = 0; i < m; i++) {
+        tau_mod[i] = tau[i] + delta;
+    }
+
+    double estim_v = noise + machineDM::stewart_value_function(z_mod, alpha, beta, lambda, tau_mod);
+    return estim_v;
+}
+double machineDM::Rand_normal(double mean, double sd)
+/** REFERENCES (http://www.cast.uark.edu/~kkvamme/ACN37.htm)
+
+ Hays, W.L.
+ 1988 Statistics (4th ed.). Holt, Rinehart and Winston, New York.
+
+ Hodder, Ian (ed.)
+ 1978 Simulation Studies in Archaeology. Cambridge University Press, Cambridge.
+
+ Olkin, I., L.J. Gleser, and C. Derman
+ 1980 Probability Models and Applications. Macmillan Publishing, New York.
+
+ Ross, Sheldon M.
+ 1989 Introduction to Probability Models (4th ed.). Academic Press, Boston.
+**/
+
+{
+    int i;
+    double x;
+
+    for (x = 0.0, i = 0; i < 12; i++) {
+        x += std::rand() / RAND_MAX;
+    }
+    return (mean + sd * (x - 6.0));
+}
+
+vector_double machineDM::fitness(const vector_double &solution) const
 {
     // FIXME: Apply biases
     vector_double f = prob.fitness(solution);
+
     return f;
 }
 
-double
-machineDM::value(const vector_double & solution) const
+double machineDM::value(const vector_double &solution) const
 {
     vector_double f = this->fitness(solution);
     // FIXME: Apply biases
     return pref.value(f);
 }
 
-double
-machineDM::true_value(const vector_double & solution) const
+double machineDM::true_value(const vector_double &solution) const
 {
     vector_double f = prob.fitness(solution);
     return pref.value(f);
 }
 
-std::vector<size_t>
-machineDM::rank(const pagmo::population &pop) const
+std::vector<size_t> machineDM::rank(const pagmo::population &pop) const
 {
     std::vector<size_t> ranks;
     // FIXME
